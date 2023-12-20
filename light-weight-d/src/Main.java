650e233a93c124c83cb0cc8f8d7cabc1e0caabba
ArunB <arun.mbabu911@gmail.com>
2023-12-20 17:59:11 +0530

    files added to repo , initial commit
    Java code for finding well digging location where prior neighboring 3 wells have already in use and are actively consumed as underwater resource

---
+++ light-weight-d/src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+
+    public int all_checks_final_ok(int bool){
+
+
+        return 0;
+        //all parameters are ok
+        //altitude , depth , soil quality, climate , greenery etc
+
+
+    }
+
+    public int powerhouse( Boolean bool){
+
+        Boolean bool = null;
+        public final Boolean a_check = (Boolean) all_checks_final_ok(params);
+        do{
+            location_a = getLocation_well1();
+            location_b = getLocation_well2();
+            location_c = getLocation_well3();
+
+
+            centroid;
+            orthocentre;
+            incentre;
+            circumcircle;
+            excircle;
+
+            //line passing through all 3 points and intersection of excircle
+
+        }
+        while(a_check.equals( 'True'));
+
+        return bool;
+
+    }
+
+    private static int job(int parameters , private partiallocation ){
+        boolean start = true;
+
+        int macchine = powerhouse(booled);
+
+
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Hello well");
+        int  output = job(params,plocation);
+        System.out.println("Well done ! ");
+
+
+
+    }
+}
